21ee38def501a8982cb48aeaf378fe76ac9f6560
Quasikot <130239541+Quasikot@users.noreply.github.com>
2023-08-20 10:50:48 +0300

    string length prediction

---
+++ py_app/stringlen_estimation_dataset.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from screenshot import take_screenshot
+from preprocess import canny_edge_detection
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import sys
+from PyQt5.QtGui import QImage, QPainter
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, QRect  
+from PyQt5.QtGui import QFontDatabase, QFont, QColor 
+from PyQt5.QtGui import QImage, QPainter, QTransform
+from PyQt5.QtGui import QFontMetrics
+app = QtWidgets.QApplication(sys.argv)
+
+def affine_transform(image, angle, translate, scale_factor):
+
+
+    
+    width, height = image.width(), image.height()
+
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+
+    resized_image = image.scaled(new_width, new_height)
+    image.fill(Qt.white)
+    painter = QPainter(image)
+    x = (width - new_width) // 2
+    y = (height - new_height) // 2
+    painter.drawImage(x+translate[0], y+translate[1], resized_image)
+    painter.end()
+
+
+    return image
+
+def split_rectangle(rectangle, n):
+  # Splits a rectangle on n smaller rectangles of equal width.
+  if n==0:
+      return []
+  width = int(rectangle.width() / n)
+  height = rectangle.height()
+  rectangles = []
+  for i in range(n):
+    x = i * width
+    y = 0
+    width = rectangle.width() - i * width
+    height = rectangle.height()
+    rectangles.append(QRect(x, y, width, height))
+  return rectangles
+
+
+def renderString(text, font):
+       avg_word_len = 5
+       image = QImage(32*avg_word_len, 32, QImage.Format_RGB888)
+       painter = QPainter(image)
+       f = QFont(font)
+       f.setPixelSize(30)
+       painter.setFont(f)
+       painter.setPen(Qt.black)
+       painter.fillRect(image.rect(), Qt.white)
+       painter.drawText(image.rect(), Qt.AlignCenter, text)
+       
+       ##----------------------------- this code only for test
+       
+      # metrics = QFontMetrics(f)
+
+       #w = metrics.width(text)
+    
+       # x = 0
+       # y = 0
+       # n = len(text)
+       # spacing = w / n
+       # for i in range(n):
+       #   painter.drawLine(x, 0, int(x + w), image.height())
+       #   x += spacing
+       #  #-----------------------------
+       del painter
+       return image
+
+def remove_items(list_, indices):
+  # Create a new list without the items at the specified indices
+  new_list = []
+  for i, item in enumerate(list_):
+      if i not in indices:
+          new_list.append(item)
+  return new_list
+
+
+
+db = QFontDatabase()
+indexes_to_remove = [32,60,71,137,165,168,179,254,255,257,258,259]
+fonts = [font for font in db.families()]
+fonts = remove_items(fonts, indexes_to_remove)
+fonts_for_train = fonts
+fonts_for_test = fonts[5:10]
+  
+# train data
+word_dictionary = {}
+i = 0
+filename = "alice_in_wonderland.txt"
+
+
+
+# data with transformations   
+with open(filename, "r", encoding='utf-8') as f:
+ for line in f:
+   for word in line.split():
+    for font in fonts_for_train:
+        word = word.lower()
+        if word not in word_dictionary:
+            word_dictionary[word] = 0
+            image = renderString(word, font)  
+            label = len(word)
+            image.save(f"string_estimation_data\{label}_{i}.png")
+            translate = (random.randint(0, int(image.width()/10)), random.randint(0, int(image.height()/10)))
+            scale = 0.7 + random.random() * 0.3
+            new_image = affine_transform(image, 0, translate, scale)
+            new_image.save(f"string_estimation_data\{label}_{i}.png")
+        word_dictionary[word] += 1   
+        i = i + 1
+ f.close()
+#image = cv2.imread("char_data\\7226.png")
+
--- py_app/train_data_collector.py
+++ py_app/train_data_collector.py
@@ -16,73 +16,98 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFontDatabase, QFont, QColor
 from PyQt5.QtGui import QImage, QPainter, QTransform
 
-def prepare_char_data():
-  # Read the image.
-  image = cv2.imread("Screenshot_english_alphabet.png")#take_screenshot()
-  image = cv2.resize(image, (image.shape[1]*4, image.shape[0]*4))
+app = QtWidgets.QApplication(sys.argv)
 
-  # Detect edges on the image.
-  edges = canny_edge_detection(image, 10, 100)
-  
-  # Find the contours in the image.
-  contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def affine_transform(image, angle, translate, scale_factor):
 
-  # Draw the filtered contours on the image.
-  index = 0
-  for contour in contours:
-      # Generate a random color for each contour.
-    random_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    bounding_rect = cv2.boundingRect(contour)
-    x, y, width, height = bounding_rect
-    cropped_image = image[y:y + height, x:x + width]
-    #cv2.imwrite(f"char_data\{index}.png", cropped_image)
-    cv2.drawContours(image, [contour], 0, random_color, 1)
-    index = index + 1
+
     
-  cv2.imwrite("contours.png", image)
+    width, height = image.width(), image.height()
+
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+
+    resized_image = image.scaled(new_width, new_height)
+    image.fill(Qt.white)
+    painter = QPainter(image)
+    x = (width - new_width) // 2
+    y = (height - new_height) // 2
+    painter.drawImage(x+translate[0], y+translate[1], resized_image)
+    painter.end()
 
 
-def affine_transform(image, angle, translate):
-  transform = QTransform()
-  transform.rotate(angle)
-  transform.translate(translate[0], translate[1])
+    return image
 
-  new_image = QImage(image.size(), QImage.Format_RGB888)
-  painter = QPainter(new_image)
-  painter.setTransform(transform)
-  painter.fillRect(new_image.rect(), Qt.white)
-  painter.drawImage(0, 0, image)
+def renderChar(character, font):
+    
+       image = QImage(32, 32, QImage.Format_RGB888)
+       painter = QPainter(image)
+       f = QFont(font)
+       f.setPixelSize(30)
+       painter.setFont(f)
+       painter.setPen(Qt.black)
+       painter.fillRect(image.rect(), Qt.white)
+       painter.drawText(image.rect(), Qt.AlignCenter, character)
+       del painter
+       return image
 
-  return new_image
-  
+def remove_items(list_, indices):
+  # Create a new list without the items at the specified indices
+  new_list = []
+  for i, item in enumerate(list_):
+      if i not in indices:
+          new_list.append(item)
+  return new_list
+
 def prepare_char_data_qt():
     chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+={}[]:;<>,.?/'
-    app = QtWidgets.QApplication(sys.argv)
+    #chars = 'abf'
+    
     db = QFontDatabase()
+    indexes_to_remove = [32,60,71,137,165,168,179,254,255,257,258,259]
     fonts = [font for font in db.families()]
-    print(fonts)
+    fonts = remove_items(fonts, indexes_to_remove)
+    fonts_for_train = fonts[0:5] + fonts[10:]
+    fonts_for_test = fonts[5:10]
+  
     i = 0
     character_index = 0
-    image = QImage(32, 32, QImage.Format_RGB888)
-    painter = QPainter(image)
+   
+    # train data
     for character in chars:
-        #print(character)
-        for font in fonts:
-          f = QFont(font)
-          f.setPixelSize(30)
-          painter.setFont(f)
-          painter.setPen(Qt.black)
-          painter.fillRect(image.rect(), Qt.white)
-          painter.drawText(image.rect(), Qt.AlignCenter, character)
-          if i < len(fonts)-10:
-              image.save(f"char_data\{character_index}_{i}.png")
-          else:
-              angle = random.randint(-5, 5)
-              translate = (random.randint(0, image.width()/2), random.randint(0, image.height()/2))
-              new_image = affine_transform(image, angle, translate)
-              image.save(f"test_data\{character_index}_{i}.png")
+        for font in fonts_for_train:
+          image = renderChar(character, font)  
+          image.save(f"char_data\{character_index}_{i}.png")
           i = i + 1
         character_index = character_index + 1
+   
+    # train data with affine transforms
+    character_index = 0
+    fonts_for_train = fonts_for_train + fonts_for_train
+    for character in chars:
+        for font in fonts_for_train:
+          image = renderChar(character, font)
+          translate = (random.randint(0, int(image.width()/10)), random.randint(0, int(image.height()/10)))
+          scale = 0.7 + random.random() * 0.3
+          new_image = affine_transform(image, 0, translate, scale)
+  
+          new_image.save(f"char_data\{character_index}_{i}.png")
+          i = i + 1
+        character_index = character_index + 1
+        
+    # test data
+    i = 0
+    character_index = 0
+    for character in chars:
+        for font in fonts_for_test:
+            image = renderChar(character, font)
+            angle = random.randint(-5, 5)
+            translate = (random.randint(0, int(image.width()/10)), random.randint(0, int(image.height()/10)))
+            new_image = affine_transform(image, angle, translate, 1)
+            new_image.save(f"test_data\{character_index}_{i}.png")
+            i = i + 1
+        character_index = character_index + 1
+
 #prepare_char_data()
 prepare_char_data_qt()
 #image = cv2.imread("char_data\\7226.png")
