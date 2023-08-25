1cece8481db43b0d4f3b3666415429ce4d97dc36
Quasikot <130239541+Quasikot@users.noreply.github.com>
2023-08-25 22:01:22 +0300

    rewrited take_screenshot

--- py_app/screenshot.py
+++ py_app/screenshot.py
@@ -3,17 +3,12 @@
 
 import sys
 import cv2
-from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtGui import QImage
-
+import pyautogui
 
 def take_screenshot():
   """Takes a screenshot of the entire screen and saves it to a file."""
 
-  #app = QtWidgets.QApplication(sys.argv)
-  app = QtWidgets.QApplication.instance()
-  screen = app.primaryScreen()
-  screenshot = QImage(screen.grabWindow(app.desktop().winId()))
+  screenshot = pyautogui.screenshot()
   screenshot.save("screenshot.png", "png")
   mat = cv2.imread("screenshot.png")
  # mat = mat.reshape(screenshot.height(), screenshot.width(), screenshot.depth() // 8)
